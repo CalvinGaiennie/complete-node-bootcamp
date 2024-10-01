2b4b08eec9adbb3c49d7551f66e9680ee7f83c0c
CalvinGaiennie <calvingaiennie@gmail.com>
2024-10-01 15:28:19 -0500

    Watched a few lectures

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,26 +1,27 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 /////////////////////////////////////////////////
 //Files
 //Blocking synchronous way
-// const textIn = fs.readFileSync("./starter/txt/input.txt", "utf-8");
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textIn);
 
 // const textOut = `This is what we know about the avacado: ${textIn}.\nCreated on ${Date.now()}`;
 
-// fs.writeFileSync("./starter/txt/output.txt", textOut);
+// fs.writeFileSync("./txt/output.txt", textOut);
 // console.log("File Written");
 
 //Non-blocking, asynchronous way
-// fs.readFile("./starter/txt/start.txt", "utf-8", (err, data1) => {
-//   fs.readFile(`./starter/txt/${data1}.txt`, "utf-8", (err, data2) => {
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
 //     console.log(data2);
-//     fs.readFile(`./starter/txt/append.txt`, "utf-8", (err, data3) => {
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
 //       console.log(data3);
 
 //       fs.writeFile(
-//         "./starter/txt/final.txt",
+//         "./txt/final.txt",
 //         `${data2}\n${data3}`,
 //         "utf-8",
 //         (err) => {
@@ -34,45 +35,28 @@ const url = require("url");
 
 //////////////////////////////////////////////
 //Server
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-  return output;
-};
 
 const tempOverview = fs.readFileSync(
-  `${__dirname}/starter/templates/template-overview.html`,
+  `${__dirname}/templates/template-overview.html`,
   "utf-8"
 );
 const tempCard = fs.readFileSync(
-  `${__dirname}/starter/templates/template-card.html`,
+  `${__dirname}/templates/template-card.html`,
   "utf-8"
 );
 const tempProduct = fs.readFileSync(
-  `${__dirname}/starter/templates/template-product.html`,
+  `${__dirname}/templates/template-product.html`,
   "utf-8"
 );
-const data = fs.readFileSync(
-  `${__dirname}/starter/dev-data/data.json`,
-  "utf-8"
-);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   // Overview page
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
     const cardsHtml = dataObj
@@ -83,11 +67,14 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     //Product Page
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT");
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     // API
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
 
