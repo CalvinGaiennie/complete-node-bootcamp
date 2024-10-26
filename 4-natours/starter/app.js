ca12c75a88a607df606c83c6483945acc5466397
CalvinGaiennie <calvingaiennie@gmail.com>
2024-10-26 12:15:39 -0500

    continued course

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,17 +1,29 @@
 const express = require('express');
 
+const morgan = require('morgan');
+
+const tourRouter = require('./Routes/tourRoutes');
+const userRouter = require('./Routes/userRoutes');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+// 2)MIDLLEWARES
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+
+// 3) Routes
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
