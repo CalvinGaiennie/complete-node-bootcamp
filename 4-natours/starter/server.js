b03179f988717466ca6ccd5b12fb6e8d48e1ce25
CalvinGaiennie <calvingaiennie@gmail.com>
2024-10-28 23:19:41 -0500

    more work

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -7,43 +7,9 @@ const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD,
 );
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('DB connection succesful!');
-  });
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: { type: Number, default: 4.5 },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
+mongoose.connect(DB, {}).then(() => {
+  console.log('DB connection succesful!');
 });
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR:', err);
-  });
 
 const app = require('./app');
 
